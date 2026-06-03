82178555f6d27eb8bcfd7631060000cd1f7e327f
Wesleywwo <wesleyww018@gmail.com>
2026-06-03 11:11:42 -0300

    01 03-06

---
+++ AtividadeMapa/application/views/Index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="../assets/css/reset.css">
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js" defer></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" defer></script>
+
+    <title>Home - Sitema Mapa de Sala</title>
+    <link rel="icon" href="../assets/img/icone_fatecSR.ico" type="image/x-icon">
+</head>
+
+<body>
+    <a id="linkHome" href="#">
+        <header>
+            <h1 id="headerTitle">Mapeamento de Salas</h1>
+        </header>
+    </a>
+    <main>
+        <section class="secao4" id="sobre">
+            <div class="secao4-div">
+                <a href="../funcoes/abreSala" class="secao4-div-card card">
+                    <img decoding="async" src="../assets/img/sala-de-aula.png" alt="imagem do card 1 sala">
+                    <h3>Sala de Aula</h3>
+                    <p>Clique para Cadastrar, Editar, Consultar ou Excluir uma sala de aula.</p>
+                </a>
+
+                <a href="../funcoes/abreProfessor" class="secao4-div-card card">
+                    <img decoding="async" src="../assets/img/professores.png" alt="imagem do card 2 professores">
+                    <h3>Docente</h3>
+                    <p>Clique para Cadastrar, Editar, Consultar ou Excluir um(a) novo(a) docente.</p>
+                </a>
+
+                <a href="../Funcoes/abreTurma" class="secao4-div-card card">
+                    <img decoding="async" src="../assets/img/turma.png" alt="imagem do card 3 turma">
+                    <h3>Turma</h3>
+                    <p>Clique para Cadastrar, Editar, Consultar ou Excluir uma nova turma.</p>
+                </a>
+
+                <a href="../Funcoes/abrePeriodo" class="secao4-div-card card">
+                    <img decoding="async" src="../assets/img/periodo.png" alt="imagem do card 4 período">
+                    <h3>Período</h3>
+                    <p>Clique para Cadastrar, Editar, Consultar ou Excluir um novo período.</p>
+                    </p>
+                </a>
+
+                <a href="../Funcoes/abreMapa" class="secao4-div-card card">
+                    <img decoding="async" src="../assets/img/mapeamento.png" alt="imagem do card 5 mapeamento">
+                    <h3>Reservas</h3>
+                    <p>Clique para Cadastrar, Editar, Consultar ou Excluir uma Reserva de Sala.</p>
+                </a>
+
+                <a href="../Funcoes/abreRelatorio" class="secao4-div-card card">
+                    <img decoding="async" src="../assets/img/relatorio.png" alt="imagem do card 6 relatório">
+                    <h3>Relatório</h3>
+                    <p>Clique para gerar Relatório de Mapeamento de Sala.</p>
+                </a>
+
+                <a href="../Funcoes/encerraSistema" class="secao4-div-card card">
+                    <img decoding="async" src="../assets/img/sair.png" alt="imagem do card 7 sair">
+                    <h3>Sair do Sistema</h3>
+                    <p>Encerrar.</p>
+                </a>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
