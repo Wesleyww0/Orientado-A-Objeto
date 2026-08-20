c6d298b19796ee806b29ffe0d6b23fbca8eae1ab
Wesleywwo <wesleyww018@gmail.com>
2026-08-20 10:30:13 -0300

    viaCep

--- Hook-main/flutter_application_1/backend/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-header('Content-Type: application/json; charset=utf-8');
-
-echo json_encode([
-    'sucesso' => true,
-    'mensagem' => 'Backend do Hook funcionando!',
-    'versao' => '1.0'
-]);
